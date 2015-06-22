57bc69d66dd6796fd7cc29e6d3cd541363206fb2
John M Buscemi <jmbuscemi@gmail.com>
2015-06-22 17:28:53 -0400

    Optimize database

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,11 +2,10 @@ class ReportsController < ApplicationController
   def all_data
     @assembly = Assembly.find_by_name(params[:name])
     @hits = []
-    @assembly.sequences.each do |s|
-      s.genes.each do |g|
-        g.hits.each do |h|
-          @hits << h
-        end
+    genes = Gene.joins(sequence: [:assembly]).where('assemblies.name ="'+"#{params[:name]}"+'"')
+    genes.each do |g|
+      g.hits.each do |h|
+        @hits << h
       end
     end
     @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
